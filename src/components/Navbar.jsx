d2155fc7f00ae70d0f81b6393132059e5a1eb009
Nati Jaimes <natalia.jaimes@hotmail.com>
2024-10-18 22:08:53 -0300

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './navBar.css'
+
+const NavBar = () => {
+    return (
+        <div className="nav">
+            <h1>NavBar</h1>
+            <a href="">1</a>
+            <a href="">2</a>
+            <a href="">3</a>
+            <a href="">4</a>
+        </div>
+    );
+};
+
+export default NavBar;
